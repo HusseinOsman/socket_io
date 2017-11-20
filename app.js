ce91fec04e6ac4b1eef4088932239dbe32a65fac
husseinosman <hussein.osman.fg@gmail.com>
2017-11-20 16:27:55 +0200

    Socket.IO - Event Handling

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ app.get('/', function(req, res) {
 
 io.on('connection',function(socket){
 	console.log('A user connected');
+
+	//send a msg after a timeout 4 sec
+	setTimeout(function(){
+		//socket.send('sent a msg 4 sec after connection');
+		socket.emit('serverEvent',{desc:'a custom event'});
+	},4000);
+
+	socket.on('clientEvent',function(data){
+		console.log('clientEvent data',data);
+	});
 	
 	//wherever someone disconnects this piece of code executed
 	socket.on('disconnect',function(){
